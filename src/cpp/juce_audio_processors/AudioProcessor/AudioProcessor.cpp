194c67d130f932346f510a6a4730da552ed6313b
Adam Szokalski <szokalskiadam@gmail.com>
2024-01-03 21:46:01 +0100

    clean imports

--- src/cpp/juce_audio_processors/AudioProcessor/AudioProcessor.cpp
+++ src/cpp/juce_audio_processors/AudioProcessor/AudioProcessor.cpp
@@ -1,7 +1,5 @@
 #include "AudioProcessor.h"
 #include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_audio_basics/juce_audio_basics.h> // for juce::AudioBuffer
-#include <juce_audio_devices/juce_audio_devices.h>
 
 class PyAudioProcessor : public juce::AudioProcessor
 {
--- src/cpp/juce_audio_processors/juce_audio_processors.cpp
+++ src/cpp/juce_audio_processors/juce_audio_processors.cpp
@@ -7,8 +7,7 @@
 #include "AudioProcessorEditor/AudioProcessorEditor.h"
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_audio_basics/juce_audio_basics.h> // for juce::AudioBuffer
-#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_basics/juce_audio_basics.h>
 
 
 

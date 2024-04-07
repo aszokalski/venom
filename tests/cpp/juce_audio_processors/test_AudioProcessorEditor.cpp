3c22a2c4ce89b3c7b04afb96b1334039fd367525
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-04-07 14:29:21 +0200

    fix: tests - change to pybind11_add_module

--- tests/cpp/juce_audio_processors/test_AudioProcessorEditor.cpp
+++ tests/cpp/juce_audio_processors/test_AudioProcessorEditor.cpp
@@ -1,21 +1,20 @@
 #include <gtest/gtest.h>
 #include <pybind11/embed.h>
 #include "juce_audio_devices/juce_audio_devices.h"
-//#include "PyAudioProcessor.h"
 
-//namespace py = pybind11;
+namespace py = pybind11;
 
 TEST(AUDIO_PROCESSOR, AudioProcessorEditorCreation){
     EXPECT_EQ(1, 1);
-//    juce::ScopedJuceInitialiser_GUI libraryInitialiser;
-//    auto interpreter = std::make_unique<py::scoped_interpreter>();
-//    auto path = py::module_::import("sys").attr("path");
-//    path.attr("append")(MODULES_DIR);
-//    path.attr("append")(STUBS_DIR);
-//    py::eval_file(PYTHON_STUBS_FILE);
-//    auto obj = py::eval("PyAudioProcessorEditor(PyAudioProcessor())");
-//
-//    juce::Image image(juce::Image::ARGB, 200, 400, true);
-//    juce::Graphics g(image);
-//    obj.attr("paint")(g);
+    juce::ScopedJuceInitialiser_GUI libraryInitialiser;
+    auto interpreter = std::make_unique<py::scoped_interpreter>();
+    auto path = py::module_::import("sys").attr("path");
+    path.attr("append")(MODULES_DIR);
+    path.attr("append")(STUBS_DIR);
+    py::eval_file(PYTHON_STUBS_FILE);
+    auto obj = py::eval("PyAudioProcessorEditor(PyAudioProcessor())");
+
+    juce::Image image(juce::Image::ARGB, 200, 400, true);
+    juce::Graphics g(image);
+    obj.attr("resized")();
 }

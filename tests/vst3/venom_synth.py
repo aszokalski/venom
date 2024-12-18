ebf8b0681543b98a4b78e818a4c982ef2a81072c
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-12-18 14:07:44 +0100

    added venom synth template

--- tests/vst3/venom_synth.py
+++ tests/vst3/venom_synth.py
@@ -1,6 +1,14 @@
+import gc
+
 import numpy as np
 from audio_basics.juce_audio_basics import MidiMessage
 from audio_processor.juce_audio_processors import MidiBuffer
+from audio_basics.juce_audio_basics import (Synthesiser,
+                                            SynthesiserSound, SynthesiserVoice)
+from audio_processor.juce_audio_processors import (AudioProcessor,
+                                                   AudioProcessorEditor,
+                                                   Colour)
+from ui_basics.ui_basics import Slider
 
 
 class synth:
@@ -78,3 +86,78 @@ class synth:
         # for message in midiMessages:
         #     self.synth.handle_midi_message(message)
         self.process(buffer)
+
+
+gc.disable()
+class SineWaveVoice(SynthesiserVoice):
+    def __init__(self):
+        super().__init__()
+        self.frequency = 440.0
+        self.amplitude = 0.5
+        self.phase = 0.0
+
+    def startNote(self, midiNoteNumber, velocity, sound, pitch):
+        pass
+
+    def stopNote(self, velocity, allowTailOff):
+        pass
+
+    def pitchWheelMoved(self, newValue):
+        pass
+
+    def controllerMoved(self, controllerNumber, newValue):
+        pass
+
+    def renderNextBlock(self, buffer, start_sample, num_samples):
+        pass
+
+
+class SineWaveSound(SynthesiserSound):
+    def __init__(self):
+        super().__init__()
+
+    def appliesToNote(self, midiNoteNumber):
+        return True
+
+    def appliesToChannel(self, midiChannel):
+        return True
+
+
+class PyAudioProcessorEditor(AudioProcessorEditor):
+    def __init__(self, processor):
+        super().__init__(processor)
+        self.setSize(400, 400)
+        self.slider = Slider()
+        self.slider.setBounds(20, 20, 200, 20)
+        self.addAndMakeVisible(self.slider, 1)
+
+
+class PySynth(AudioProcessor):
+    def __init__(
+        self,
+        sample_rate: int = 44100,
+        waveform: str = "sine",
+        voices: int = 4,
+        sound: SynthesiserSound = SineWaveSound(),
+    ):
+        super().__init__()
+        self.sample_rate = sample_rate
+        self.waveform = waveform
+        self.frequency = 440.0
+        self.amplitude = 0.5
+        self.phase = 0.0
+        self.note_on = False
+        self.current_note = None
+        self.synth = Synthesiser()
+
+        for _ in range(voices):
+            self.synth.addVoice(SineWaveVoice())
+        self.synth.addSound(sound)
+
+    def processBlock(self, buffer, midiMessages):
+        buffer.clear()
+        self.synth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples())
+
+    def createEditor(self):
+        return PyAudioProcessorEditor(self)
+

8695f42a1456caa3caab69a24f67c798888a0921
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-12-21 13:20:52 +0100

    added create parameter layout

--- venom/juce/juce_audio_processors/AudioProcessor/AudioProcessor.cpp
+++ venom/juce/juce_audio_processors/AudioProcessor/AudioProcessor.cpp
@@ -1,7 +1,7 @@
 #include "AudioProcessor.h"
 
 class PyAudioProcessor : public juce::AudioProcessor {
-public:
+   public:
     using juce::AudioProcessor::AudioProcessor;
 
     void prepareToPlay(double sampleRate, int samplesPerBlock) override {
@@ -29,28 +29,28 @@ public:
     using juce::AudioProcessor::processBlock;
 
     juce::AudioProcessorEditor *createEditor() override {
-        PYBIND11_OVERRIDE_PURE(juce::AudioProcessorEditor *, juce::AudioProcessor, createEditor,);
+        PYBIND11_OVERRIDE_PURE(juce::AudioProcessorEditor *, juce::AudioProcessor, createEditor, );
     }
 
-    bool hasEditor() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, hasEditor,); }
+    bool hasEditor() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, hasEditor, ); }
 
     const juce::String getName() const override {
-        PYBIND11_OVERRIDE_PURE(juce::String, juce::AudioProcessor, getName,);
+        PYBIND11_OVERRIDE_PURE(juce::String, juce::AudioProcessor, getName, );
     }
 
-    bool acceptsMidi() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, acceptsMidi,); }
+    bool acceptsMidi() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, acceptsMidi, ); }
 
-    bool producesMidi() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, producesMidi,); }
+    bool producesMidi() const override { PYBIND11_OVERRIDE_PURE(bool, juce::AudioProcessor, producesMidi, ); }
 
-    bool isMidiEffect() const override { PYBIND11_OVERRIDE(bool, juce::AudioProcessor, isMidiEffect,); }
+    bool isMidiEffect() const override { PYBIND11_OVERRIDE(bool, juce::AudioProcessor, isMidiEffect, ); }
 
     double getTailLengthSeconds() const override {
-        PYBIND11_OVERRIDE_PURE(double, juce::AudioProcessor, getTailLengthSeconds,);
+        PYBIND11_OVERRIDE_PURE(double, juce::AudioProcessor, getTailLengthSeconds, );
     }
 
-    int getNumPrograms() override { PYBIND11_OVERRIDE_PURE(int, juce::AudioProcessor, getNumPrograms,); }
+    int getNumPrograms() override { PYBIND11_OVERRIDE_PURE(int, juce::AudioProcessor, getNumPrograms, ); }
 
-    int getCurrentProgram() override { PYBIND11_OVERRIDE_PURE(int, juce::AudioProcessor, getCurrentProgram,); }
+    int getCurrentProgram() override { PYBIND11_OVERRIDE_PURE(int, juce::AudioProcessor, getCurrentProgram, ); }
 
     void setCurrentProgram(int index) override {
         PYBIND11_OVERRIDE_PURE(void, juce::AudioProcessor, setCurrentProgram, index);
@@ -71,105 +71,106 @@ public:
     void setStateInformation(const void *data, int sizeInBytes) override {
         PYBIND11_OVERRIDE_PURE(void, juce::AudioProcessor, setStateInformation, data, sizeInBytes);
     }
-};
 
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout() override {
+        PYBIND11_OVERRIDE_PURE(juce::AudioProcessorValueTreeState::ParameterLayout, juce::AudioProcessor,
+                               createParameterLayout, );
+    }
+};
 
 void init_AudioProcessor(py::module &m) {
     py::class_<juce::AudioProcessor, std::shared_ptr<juce::AudioProcessor>, PyAudioProcessor>(m, "AudioProcessor",
                                                                                               py::dynamic_attr())
-            .def(py::init([]() {
-                return createOnMessageThread<juce::AudioProcessor>([] {
-                    return new PyAudioProcessor();
-                });
-            }))
-            .def("getName", [](juce::AudioProcessor &self) { return self.getName(); })
-            .def("prepareToPlay", [](juce::AudioProcessor &self, double sampleRate,
-                                     int samplesPerBlock) { self.prepareToPlay(sampleRate, samplesPerBlock); })
-            .def("releaseResources", [](juce::AudioProcessor &self) { self.releaseResources(); })
-            .def("isBusesLayoutSupported",
-                 [](juce::AudioProcessor &self, const juce::AudioProcessor::BusesLayout &layouts) { return true; })
-            .def("processBlock", [](juce::AudioProcessor &self, juce::AudioBuffer<float> &buffer,
-                                    juce::MidiBuffer &midiMessages) { self.processBlock(buffer, midiMessages); })
-            .def("createEditor", [](juce::AudioProcessor &self) {
-                return createOnMessageThread<juce::AudioProcessorEditor>([&self] {
-                    return self.createEditor();
-                });
-            })
-            .def("hasEditor", [](juce::AudioProcessor &self) { return self.hasEditor(); })
-            .def("acceptsMidi", [](juce::AudioProcessor &self) { return self.acceptsMidi(); })
-            .def("producesMidi", [](juce::AudioProcessor &self) { return self.producesMidi(); })
-            .def("isMidiEffect", [](juce::AudioProcessor &self) { return self.isMidiEffect(); })
-            .def("getTailLengthSeconds", [](juce::AudioProcessor &self) { return self.getTailLengthSeconds(); })
-            .def("getNumPrograms", [](juce::AudioProcessor &self) { return self.getNumPrograms(); })
-            .def("getCurrentProgram", [](juce::AudioProcessor &self) { return self.getCurrentProgram(); })
-            .def("setCurrentProgram", [](juce::AudioProcessor &self, int index) { self.setCurrentProgram(index); })
-            .def("getProgramName", [](juce::AudioProcessor &self, int index) { return self.getProgramName(index); })
-            .def("changeProgramName", [](juce::AudioProcessor &self, int index,
-                                         const juce::String &newName) { self.changeProgramName(index, newName); })
-            .def("getStateInformation",
-                 [](juce::AudioProcessor &self, juce::MemoryBlock &destData) { self.getStateInformation(destData); })
-            .def("setStateInformation", [](juce::AudioProcessor &self, const void *data, int sizeInBytes) {
-                self.setStateInformation(data, sizeInBytes);
-            });
+        .def(py::init(
+            []() { return createOnMessageThread<juce::AudioProcessor>([] { return new PyAudioProcessor(); }); }))
+        .def("getName", [](juce::AudioProcessor &self) { return self.getName(); })
+        .def("prepareToPlay", [](juce::AudioProcessor &self, double sampleRate,
+                                 int samplesPerBlock) { self.prepareToPlay(sampleRate, samplesPerBlock); })
+        .def("releaseResources", [](juce::AudioProcessor &self) { self.releaseResources(); })
+        .def("isBusesLayoutSupported",
+             [](juce::AudioProcessor &self, const juce::AudioProcessor::BusesLayout &layouts) { return true; })
+        .def("processBlock", [](juce::AudioProcessor &self, juce::AudioBuffer<float> &buffer,
+                                juce::MidiBuffer &midiMessages) { self.processBlock(buffer, midiMessages); })
+        .def("createEditor",
+             [](juce::AudioProcessor &self) {
+                 return createOnMessageThread<juce::AudioProcessorEditor>([&self] { return self.createEditor(); });
+             })
+        .def("hasEditor", [](juce::AudioProcessor &self) { return self.hasEditor(); })
+        .def("acceptsMidi", [](juce::AudioProcessor &self) { return self.acceptsMidi(); })
+        .def("producesMidi", [](juce::AudioProcessor &self) { return self.producesMidi(); })
+        .def("isMidiEffect", [](juce::AudioProcessor &self) { return self.isMidiEffect(); })
+        .def("getTailLengthSeconds", [](juce::AudioProcessor &self) { return self.getTailLengthSeconds(); })
+        .def("getNumPrograms", [](juce::AudioProcessor &self) { return self.getNumPrograms(); })
+        .def("getCurrentProgram", [](juce::AudioProcessor &self) { return self.getCurrentProgram(); })
+        .def("setCurrentProgram", [](juce::AudioProcessor &self, int index) { self.setCurrentProgram(index); })
+        .def("getProgramName", [](juce::AudioProcessor &self, int index) { return self.getProgramName(index); })
+        .def("changeProgramName", [](juce::AudioProcessor &self, int index,
+                                     const juce::String &newName) { self.changeProgramName(index, newName); })
+        .def("getStateInformation",
+             [](juce::AudioProcessor &self, juce::MemoryBlock &destData) { self.getStateInformation(destData); })
+        .def("setStateInformation", [](juce::AudioProcessor &self, const void *data,
+                                       int sizeInBytes) { self.setStateInformation(data, sizeInBytes); })
+        .def("createParameterLayout", [](juce::AudioProcessor &self) { return self.createParameterLayout(); });
 
     using namespace py::literals;
     py::class_<juce::AudioBuffer<float>>(m, "AudioBuffer", py::buffer_protocol())
-            .def(py::init<>())
-            .def(py::init<int, int>(), "numChannels"_a, "numSamples"_a)
-            .def("getNumChannels", &juce::AudioBuffer<float>::getNumChannels)
-            .def("getNumSamples", &juce::AudioBuffer<float>::getNumSamples)
-            .def(
-                    "getWritePointer",
-                    [](juce::AudioBuffer<float> &self, int channelNumber) {
-                        return py::array_t<float>({self.getNumSamples()}, {sizeof(float)},
-                                                  self.getWritePointer(channelNumber),
-                                                  py::cast(&self));
-                    },
-                    py::arg("channelNumber"))
-            .def("clear", py::overload_cast<>(&juce::AudioBuffer<float>::clear));
+        .def(py::init<>())
+        .def(py::init<int, int>(), "numChannels"_a, "numSamples"_a)
+        .def("getNumChannels", &juce::AudioBuffer<float>::getNumChannels)
+        .def("getNumSamples", &juce::AudioBuffer<float>::getNumSamples)
+        .def(
+            "getWritePointer",
+            [](juce::AudioBuffer<float> &self, int channelNumber) {
+                return py::array_t<float>({self.getNumSamples()}, {sizeof(float)}, self.getWritePointer(channelNumber),
+                                          py::cast(&self));
+            },
+            py::arg("channelNumber"))
+        .def("clear", py::overload_cast<>(&juce::AudioBuffer<float>::clear));
 
     py::class_<juce::MidiBufferIterator>(m, "MidiBufferIterator")
-            .def(py::init<>())
-            .def(py::init([](py::bytes data) {
-                std::string str = data;
-                return juce::MidiBufferIterator(reinterpret_cast<const uint8_t*>(str.data()));
-            }))
-            .def("__iter__", [](juce::MidiBufferIterator &it) -> juce::MidiBufferIterator& { return it; })
-            .def("__next__", [](juce::MidiBufferIterator &it) {
-                auto current = *it;
-                ++it;
-                return current;
-            })
-            .def("__eq__", &juce::MidiBufferIterator::operator==)
-            .def("__ne__", &juce::MidiBufferIterator::operator!=);
+        .def(py::init<>())
+        .def(py::init([](py::bytes data) {
+            std::string str = data;
+            return juce::MidiBufferIterator(reinterpret_cast<const uint8_t *>(str.data()));
+        }))
+        .def("__iter__", [](juce::MidiBufferIterator &it) -> juce::MidiBufferIterator & { return it; })
+        .def("__next__",
+             [](juce::MidiBufferIterator &it) {
+                 auto current = *it;
+                 ++it;
+                 return current;
+             })
+        .def("__eq__", &juce::MidiBufferIterator::operator==)
+        .def("__ne__", &juce::MidiBufferIterator::operator!=);
 
     py::class_<juce::MidiBuffer>(m, "MidiBuffer")
-            .def(py::init<>())
-            .def(py::init<const juce::MidiMessage&>())
-            
-            .def("clear", py::overload_cast<>(&juce::MidiBuffer::clear))
-            .def("clear", py::overload_cast<int, int>(&juce::MidiBuffer::clear))
-            .def("isEmpty", &juce::MidiBuffer::isEmpty)
-            .def("getNumEvents", &juce::MidiBuffer::getNumEvents)
-            
-            .def("addEvent", py::overload_cast<const juce::MidiMessage&, int>(&juce::MidiBuffer::addEvent))
-            .def("addEvent", [](juce::MidiBuffer& self, py::bytes rawData, int sampleNumber) {
-                std::string data = rawData;
-                return self.addEvent(data.data(), static_cast<int>(data.size()), sampleNumber);
-            })
-            .def("addEvents", &juce::MidiBuffer::addEvents)
-            
-            .def("getFirstEventTime", &juce::MidiBuffer::getFirstEventTime)
-            .def("getLastEventTime", &juce::MidiBuffer::getLastEventTime)
-            
-            .def("swapWith", &juce::MidiBuffer::swapWith)
-            .def("ensureSize", &juce::MidiBuffer::ensureSize)
-            
-            .def("begin", &juce::MidiBuffer::begin)
-            .def("end", &juce::MidiBuffer::end)
-            .def("cbegin", &juce::MidiBuffer::cbegin)
-            .def("cend", &juce::MidiBuffer::cend)
-            .def("findNextSamplePosition", &juce::MidiBuffer::findNextSamplePosition);
+        .def(py::init<>())
+        .def(py::init<const juce::MidiMessage &>())
+
+        .def("clear", py::overload_cast<>(&juce::MidiBuffer::clear))
+        .def("clear", py::overload_cast<int, int>(&juce::MidiBuffer::clear))
+        .def("isEmpty", &juce::MidiBuffer::isEmpty)
+        .def("getNumEvents", &juce::MidiBuffer::getNumEvents)
+
+        .def("addEvent", py::overload_cast<const juce::MidiMessage &, int>(&juce::MidiBuffer::addEvent))
+        .def("addEvent",
+             [](juce::MidiBuffer &self, py::bytes rawData, int sampleNumber) {
+                 std::string data = rawData;
+                 return self.addEvent(data.data(), static_cast<int>(data.size()), sampleNumber);
+             })
+        .def("addEvents", &juce::MidiBuffer::addEvents)
+
+        .def("getFirstEventTime", &juce::MidiBuffer::getFirstEventTime)
+        .def("getLastEventTime", &juce::MidiBuffer::getLastEventTime)
+
+        .def("swapWith", &juce::MidiBuffer::swapWith)
+        .def("ensureSize", &juce::MidiBuffer::ensureSize)
+
+        .def("begin", &juce::MidiBuffer::begin)
+        .def("end", &juce::MidiBuffer::end)
+        .def("cbegin", &juce::MidiBuffer::cbegin)
+        .def("cend", &juce::MidiBuffer::cend)
+        .def("findNextSamplePosition", &juce::MidiBuffer::findNextSamplePosition);
 
     py::class_<juce::String>(m, "String").def(py::init<>()).def(py::init([](const char *text) {
         return new juce::String(text);

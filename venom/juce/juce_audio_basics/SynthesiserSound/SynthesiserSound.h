4b5cb1d6f9e09f546ee5f402a73eb01434d41ad3
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-12 14:43:56 +0100

    added message manager helper

--- venom/juce/juce_audio_basics/SynthesiserSound/SynthesiserSound.h
+++ venom/juce/juce_audio_basics/SynthesiserSound/SynthesiserSound.h
@@ -2,6 +2,7 @@
 #define SYNTHESISERSOUND_H
 #define PYBIND11_DETAILED_ERROR_MESSAGES
 
+#include "MessageManagerHelper.h"
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>

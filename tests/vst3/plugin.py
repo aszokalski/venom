6f3d9b90ec5e6bc27cdb5f77bb675b23add2a9be
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-12 16:35:24 +0100

    reverted

--- tests/vst3/plugin.py
+++ tests/vst3/plugin.py
@@ -1,14 +1,14 @@
 import sys
 import os
 import gc
-from venom.wrapper.audio.processors.VAudioProcessor import VAudioProcessor
-from venom.wrapper.audio.processors.VAudioProcessorEditor import VAudioProcessorEditor
+from audio_processor.juce_audio_processors import AudioProcessor, AudioProcessorEditor, Colour
 from ui_basics.ui_basics import Slider
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import venom_effects
+import venom_synth
 
 gc.disable()
-class PyAudioProcessorEditor(VAudioProcessorEditor):
+class PyAudioProcessorEditor(AudioProcessorEditor):
     def __init__(self, processor):
         super().__init__(processor)
         self.setSize(400, 400)
@@ -16,7 +16,7 @@ class PyAudioProcessorEditor(VAudioProcessorEditor):
         self.slider.setBounds(20, 20, 200, 20)
         self.addAndMakeVisible(self.slider, 1)
 
-class PyAudioProcessor(VAudioProcessor):
+class PyAudioProcessor(AudioProcessor):
     def __init__(self):
         super().__init__()
         self.sample_rate = 44100
@@ -31,3 +31,42 @@ class PyAudioProcessor(VAudioProcessor):
 
     def createEditor(self):
         return PyAudioProcessorEditor(self)
+
+    def hasEditor(self):
+        return True
+
+    def getName(self):
+        return "PyAudioProcessor"
+
+    def acceptsMidi(self):
+        return True
+
+    def isMidiEffect(self):
+        return False
+
+    def producesMidi(self):
+        return False
+
+    def getTailLengthSeconds(self):
+        return 0
+
+    def getNumPrograms(self):
+        return 0
+
+    def getCurrentProgram(self):
+        return 0
+    
+    def setCurrentProgram(self, index):
+        pass
+
+    def getProgramName(self, index):
+        return ""
+
+    def changeProgramName(self, index, newName):
+        pass
+
+    def getStateInformation(self, memoryBlock):
+        pass
+
+    def setStateInformation(self, data, sizeInBytes):
+        pass

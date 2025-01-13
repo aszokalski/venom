5d40874b49575d90cd8c0344ada2a1b19c5aaeed
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-13 21:43:06 +0100

    reverted effects examples

--- tests/vst3/plugin.py
+++ tests/vst3/plugin.py
@@ -5,7 +5,6 @@ from audio_processor.juce_audio_processors import AudioProcessor, AudioProcessor
 from ui_basics.ui_basics import Slider
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import venom_effects
-import venom_synth
 
 gc.disable()
 class PyAudioProcessorEditor(AudioProcessorEditor):

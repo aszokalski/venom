9a74054b81af8e4492263a9999eb5295c0e75b14
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-15 19:42:37 +0100

    changed from where to copy files

--- venom/builder/tools/project.py
+++ venom/builder/tools/project.py
@@ -1,36 +1,12 @@
 import os
 import shutil
-import sys
 
 from tqdm import tqdm
 from pathlib import Path
 
 from venom.builder.tools import cmake
+from venom.builder.tools.template import is_present, new_project
 from venom.builder.tools.utils.Config import Config
-
-
-def modify_cmake_lists(source_path: str, config: Config):
-    with open(os.path.join(source_path, "build", "CMakeLists.txt"), "r") as file_handle:
-        lines = file_handle.readlines()
-        for index, line in enumerate(lines):
-            if "set(PLUGIN_NAME" in line:
-                lines[index] = "set(PLUGIN_NAME " + config.name + ")\n"
-            if "set(PLUGIN_VERSION" in line:
-                lines[index] = "set(PLUGIN_VERSION " + config.version + ")\n"
-            if "set(PLUGIN_AUTHOR" in line:
-                lines[index] = "set(PLUGIN_AUTHOR " + config.author + ")\n"
-            if "set(FORMATS" in line:
-                lines[index] = "set(FORMATS " + " ".join(config.targets) + ")\n"
-            if "set(SITE_PACKAGES_DIRS" in line:
-                site_packages_dirs = [
-                    f'"{path}"' for path in sys.path if "site-packages" in path
-                ]
-                lines[index] = (
-                    "set(SITE_PACKAGES_DIRS " + " ".join(site_packages_dirs) + ")\n"
-                )
-
-    with open(os.path.join(source_path, "build", "CMakeLists.txt"), "w") as file_handle:
-        file_handle.writelines(lines)
 
 
 def build_project(source_path: str, p_bar: tqdm, cmake_args: list = []) -> None:
@@ -38,69 +14,43 @@ def build_project(source_path: str, p_bar: tqdm, cmake_args: list = []) -> None:
     _source_path = Path(source_path)
     p_bar.refresh()
 
-    with (_source_path / "venom.yaml").open() as file_handle:
+    if not is_present(source_path):
         p_bar.set_description("Copying boilerplate project")
         p_bar.refresh()
+        new_project(source_path, "VenomPlugin", "VenomAuthor", ["Standalone", "VST3"])
+
+    with (_source_path / "venom.yaml").open() as file_handle:
         config = Config.from_yaml(file_handle)
-        # Create dist directory as a symbolic link
-        (_source_path / "dist").symlink_to(
-            _source_path / "build" / f"{config.name}_artefacts",
-        )
 
-        # copy boilerplate project
-        if not (_source_path / "build").exists():
-            shutil.copytree(
-                Path(__file__).parent.parent / "boilerplate_plugin_project",
-                _source_path / "build",
-                ignore=shutil.ignore_patterns(
-                    "__pycache__", "*.pyc", "build", "tests", "docs", "examples"
-                ),
-            )
+    if (_source_path / "build").exists():
+        p_bar.set_description("Cleaning previous build directory")
+        shutil.rmtree(_source_path / "build")
+        shutil.rmtree(_source_path / "dist")
+    p_bar.set_description("Creating build directory")
+    (_source_path / "dist").symlink_to(
+        _source_path / "build" / f"{config.name}_artefacts",
+    )
 
-        source_dir = Path(__file__).parent.parent / "boilerplate_plugin_project"
-        destination_dir = _source_path / "build"
-        files_to_copy = [
-            "CMakeLists.txt",
-            "create_plugin.cpp",
-            "plugin.py",
-            "requirements.txt",
-        ]
+    p_bar.update(2)
+    p_bar.set_description("Initializing CMake")
+    p_bar.refresh()
 
-        destination_dir.mkdir(exist_ok=True)
+    # Initialize CMake with the given arguments
+    cmake_args = [
+        f"-DPLUGIN_NAME={config.name}",
+        f"-DPLUGIN_VERSION={config.version}",
+        f"-DPLUGIN_AUTHOR={config.author}",
+        f"-DENTRYPOINT={config.entrypoint}",
+        f"-DPROJECT_SOURCE_DIR={_source_path.resolve()}",
+        "-DFORMATS=VST3", #TODO: Add support for other formats
+        "-DDEFAULT_LOG_LEVEL=0", #TODO: Add other log levels
+        *cmake_args,
+    ]
 
-        # Copy each file from the source to the destination
-        for file_name in files_to_copy:
-            source_file = source_dir / file_name
-            destination_file = destination_dir / file_name
+    if "CMAKE_ARGS" in os.environ:
+        cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
-            if source_file.exists():
-                shutil.copy2(source_file, destination_file)
-                print(f"Copied {file_name} to {destination_dir}")
-            else:
-                print(f"Warning: {file_name} not found in {source_dir}")
-
-        # Setup CMakeLists.txt with config
-        modify_cmake_lists(source_path, config)
-        p_bar.update(2)
-        p_bar.set_description("Initializing CMake")
-        p_bar.refresh()
-
-        # Initialize CMake with the given arguments
-        cmake_args = [
-            f"-DPLUGIN_NAME={config.name}",
-            f"-DPLUGIN_VERSION={config.version}",
-            f"-DPLUGIN_AUTHOR={config.author}",
-            f"-DENTRYPOINT={config.entrypoint}",
-            f"-DPROJECT_SOURCE_DIR={source_path}",
-            "-DFORMATS=VST3", #TODO: Add support for other formats
-            "-DDEFAULT_LOG_LEVEL=0", #TODO: Add other log levels
-            *cmake_args,
-        ]
-
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        cmake.init(os.path.join(source_path, "build"), p_bar, cmake_args)
+    cmake.init(source_path, p_bar, cmake_args)
 
     p_bar.update(1)
     p_bar.set_description("Building CMake")
@@ -112,7 +62,7 @@ def build_project(source_path: str, p_bar: tqdm, cmake_args: list = []) -> None:
         f"{config.name}",
     ]
 
-    cmake.build_target(os.path.join(source_path, "build"), p_bar, build_args)
+    cmake.build_target(source_path, p_bar, build_args)
     p_bar.update(4)
     p_bar.set_description("Done")
     p_bar.refresh()
--- venom/builder/tools/template.py
+++ venom/builder/tools/template.py
@@ -1,7 +1,14 @@
+import shutil
+
+from pathlib import Path
 from typing import List
-import os
+
+from venom.builder.tools.utils.Config import Config
+
 def new_project(source_path: str, plugin_name: str, author_name: str, targets: List[str]):
     targets_str = "\n".join([f"  - {target}" for target in targets])
+    _source_path = Path(source_path)
+
     yaml = f"""
 name: {plugin_name}
 version: 1.0.0
@@ -10,10 +17,43 @@ entrypoint: plugin.py
 targets:
 {targets_str}
 """
-    with open(os.path.join(source_path, "venom.yaml"), "w") as f:
+    with open(_source_path / "venom.yaml", "w") as f:
         f.write(yaml)
 
-    # copy utils/example_plugin.py to source_path/plugin.py
-    example_plugin = open(os.path.join(os.path.dirname(__file__), "utils/example_plugin.py")).read()
-    with open(os.path.join(source_path, "plugin.py"), "w") as f:
-        f.write(example_plugin)
+    boilerplate_dir = Path(__file__).parent.parent / "boilerplate_plugin_project"
+    files_to_copy = [
+        "plugin.py",
+        "requirements.txt",
+    ]
+
+    # Copy each file from the source to the destination
+    for file_name in files_to_copy:
+        source_file = boilerplate_dir / file_name
+        destination_file = _source_path / file_name
+
+        if source_file.exists():
+            shutil.copy2(source_file, destination_file)
+            print(f"Copied {file_name} to {source_path}")
+        else:
+            print(f"Warning: {file_name} not found in {boilerplate_dir}")
+
+
+def is_present(source_path: str) -> bool:
+    file_path = Path(source_path) / "venom.yaml"
+
+    if not file_path.exists():
+        return False
+
+    with open(file_path) as file:
+        config = Config.from_yaml(file)
+
+    files = [
+        config.entrypoint,
+        "requirements.txt",
+    ]
+
+    for file in files:
+        if not (Path(source_path) / file).exists():
+            return False
+
+    return True
